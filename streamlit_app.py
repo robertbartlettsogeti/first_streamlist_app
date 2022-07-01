05305e33184c139ea91b3400922671e14765f4d3
Robert Bartlett <108538501+robertbartlettsogeti@users.noreply.github.com>
2022-07-01 11:53:31 -0500

    Update streamlit_app.py
    binding select box to filter dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.title('My Moms New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -12,6 +13,7 @@ streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # picklist
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display table on page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
