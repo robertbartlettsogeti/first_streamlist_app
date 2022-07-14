ae17abd61a0461bfbc063ff792067029db179d2a
Robert Bartlett <108538501+robertbartlettsogeti@users.noreply.github.com>
2022-07-14 12:43:09 -0500

    Remove json output and change request

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
 # take the JSON version of the response and normalizee it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
