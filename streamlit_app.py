ec7163a5401ad35f03ff09da9832b81642ed2fc1
Robert Bartlett <108538501+robertbartlettsogeti@users.noreply.github.com>
2022-07-14 12:41:52 -0500

    Insert dataframe on page

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) # just writes data to the screen
+
+# take the JSON version of the response and normalizee it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it to the screen as a table
+streamlit.dataframe(fruityvice_normalized)
