ed13df4ac2de75f265f6f6f4051dcf47a5ec5c85
Robert Bartlett <108538501+robertbartlettsogeti@users.noreply.github.com>
2022-07-14 12:46:03 -0500

    updated to add new select box and pass chosen fruit to service

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,9 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', fruit_choice)                                   
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) # just writes data to the screen
 
 # take the JSON version of the response and normalizee it
