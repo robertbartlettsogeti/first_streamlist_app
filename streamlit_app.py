903fb3f5448c31a4a4923192ab41c94e3574acfa
Robert Bartlett <108538501+robertbartlettsogeti@users.noreply.github.com>
2022-07-14 12:35:20 -0500

    Adding fruityvice API request response to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display table on page
 streamlit.dataframe(fruits_to_show)
+# New section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.test(fruityvice_response)
